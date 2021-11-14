bd985c68b3e29a25baba3795846c54382bd08689
Soldy <4786022+Soldy@users.noreply.github.com>
2021-11-14 23:57:37 +0000

    update
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-
-const confrc = new (require('confrc')).confrc();
-const poolrc = new (require('poolrc')).poolrc();
-const initrc = new (require('initrc')).init();
+'use strict';
+const $confrc = new (require('confrc')).base;
+const $poolrc = new (require('poolrc')).poolrc();
+const $initrc = new (require('initrc')).init();
 const http = require('http');
-exports.server = function(){
-    this.start=function(){
+
+
+const _server = function(){
+    this.start=function(_processor_){
         http.createServer(function (req, res) {
+            let _post = '';
             req.on('data', function (chunk) {
-             });
-             req.on('end', async function () {
+                _post+=chunk;
+            });
+            req.on('end', async function () {
+                _post = JSON.stringify(_post);
+                res.write(
+                    await _processor_(req, _post);
+                );
+                res.send();
             });
         }).listen(
-            confrc.get("http").port
+            confrc.get('http').port
         );
     }
     this.stop=function(){ 
         http.close();
     }
 }
+exports.base = _server;
