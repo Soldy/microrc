10617ed6d9b1842f47af517bf0f7fb3831848979
Soldy <4786022+Soldy@users.noreply.github.com>
2022-04-11 10:04:28 +0100

    temporary post error solution
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
 'use strict';
-const $confrc = new (require('confrc')).base;
-const $poolrc = new (require('poolrc')).poolrc();
-const $initrc = new (require('initrc')).init();
+const $setuprc = (require('setuprc')).base;
 const http = require('http');
 
 
-const _server = function(){
+const _server = function(settings){
     this.start=function(_processor_){
         http.createServer(function (req, res) {
             let _post = '';
             req.on('data', function (chunk) {
-                _post+=chunk;
+                if(
+                    (typeof chunk === 'string')&&
+                    (typeof chunk !== 'undefined')
+                )
+                    _post+=chunk;
             });
             req.on('end', async function () {
-                _post = JSON.stringify(_post);
+                try{
+                    _post = JSON.stringify(_post);
+                }catch(err){
+                    return console.log(err);
+                }
                 res.write(
-                    await _processor_(req, _post);
+                    await _processor_(req, _post)
                 );
-                res.send();
+                res.end();
             });
         }).listen(
-            confrc.get('http').port
+            _setup.get('port'),
+            _setup.get('address')
         );
     }
     this.stop=function(){ 
         http.close();
     }
+    /*
+     * setup  helper
+     * @private
+     */
+    let _setup = new $setuprc({
+        'address':{
+            'type'    : 'string',
+            'default' : '127.0.0.1'
+        },
+        'port' : {
+            'type'    : 'integer',
+            'default' : 8888 
+        }
+    });
+    /*
+     *
+     * setup init 
+     *
+     */
+    if(typeof settings !== 'undefined')
+        _setup.setup(settings);
 }
 exports.base = _server;
